6eeab17ade44c23495e7df03c464aa94935582ce
sophia-pham <slp227@lehigh.edu>
2025-10-14 14:40:41 -0400

    Update dff tbs

--- dff_asynchRST_tb.v
+++ dff_asynchRST_tb.v
@@ -9,14 +9,18 @@ module dff_asynchRST_tb();
 	
 	integer i;
 	initial begin
-		clk=; d=0; rstn=0;
+		clk=0; d=0; rstn=1;
 		
+		//d is 1 and rstn == 0, so q should be 1 on next posedge
 		#15 d = 1;
-		#10 rstn = 1;
-		for (i = 0; i < 5; i = i+1) begin
-			delay = $random;
-			#{delay} d = ~d;
-		end
+		//d is still 1 but rstn == 1, so q should be 0 on next posedge
+		#7 rstn = 1;
+		//d is changing while rst is still active, q -> 0
+		#9 d = 0; #4 d = 1;
+		//rstn is high again, so q should reflect d on next posedge
+		#3 rstn = 1;
+		//d is low and should output to q
+		#4 d = 0; #10;
 		$stop;
 	end
 endmodule
--- dff_synchRST_tb.v
+++ dff_synchRST_tb.v
@@ -1,22 +1,26 @@
 module dff_synchRST_tb();
-	wire d, rstn, clk;
-	reg q;
-	reg [2:0] delay;
+	reg d, rstn, clk;
+	wire q;
 	
 	dff_synchRST dut(.d(d), .rstn(rstn), .clk(clk), .q(q));
 	
-	always #10 clk = ~clk;
+	always #5 clk = ~clk;
 	
 	integer i;
 	initial begin
-		clk=; d=0; rstn=0;
+		//note that rst is active low
+		clk=0; d=0; rstn=1;
 		
+		//d is 1 and rstn == 0, so q should be 1 on next posedge
 		#15 d = 1;
-		#10 rstn = 1;
-		for (i = 0; i < 5; i = i+1) begin
-			delay = $random;
-			#{delay} d = ~d;
-		end
+		//d is still 1 but rstn == 1, so q should be 0 on next posedge
+		#7 rstn = 1;
+		//d is changing while rst is still active, q -> 0
+		#9 d = 0; #4 d = 1;
+		//rstn is high again, so q should reflect d on next posedge
+		#3 rstn = 1;
+		//d is low and should output to q
+		#4 d = 0; #10;
 		$stop;
 	end
 endmodule
